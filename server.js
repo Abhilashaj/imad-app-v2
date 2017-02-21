2589662c89fafa05bf2501f3c77aaa08f581d3d5
Abhilashaj <abhilashajumrani@gmail.com>
2017-02-21 11:16:39 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ app.get('/', function (req, res) {
 });
 
 app.get('/article-one', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui','artcle-one.html'));
+  res.sendFile(path.join(__dirname, 'ui','article-one.html'));
 });
 
 app.get('/article-two', function (req, res) {
@@ -18,7 +18,7 @@ res.sendFile(path.join(__dirname, 'ui', 'article-two.html'));
 });
 
 app.get('/article-three', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui','artcle-three.html'));
+  res.sendFile(path.join(__dirname, 'ui','article-three.html'));
 });
 
 app.get('/ui/madi.png', function (req, res) {
